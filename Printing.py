4e47c825ff0f180f3690ccfd5adc4b9341543d41
Laruno <Laman_Art@outlook.com>
2022-05-18 13:08:44 +0100

    Done

---
+++ Printing.py
@@ -0,0 +1,14 @@
+# Instructions:
+# Write a program that prints the same notes from the previous lesson using what
+# you have learnt about the Python print function
+
+# Example Output
+# When you run your Program, it should print the following:
+# Day 1 - Python Print Function
+# The function is declared lie this:
+# print('what to print')
+
+##################################### Output #####################################
+print("\nDay 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')\n")
